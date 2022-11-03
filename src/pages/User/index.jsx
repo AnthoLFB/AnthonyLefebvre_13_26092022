3c6c6f665e850a0e996c0de860add2673f435f34
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-11-03 16:46:11 +0100

    Add the component to the page.

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -8,12 +8,15 @@ import Footer from '../../components/Footer';
 
 //CSS
 import '../../styles/pages/User.css';
+import UserProfileInformation from '../../components/UserProfileInformation';
 
 function User() {
   return (
     <React.Fragment>
       <Header />
       <main className='main-user'>
+        <UserProfileInformation name={"Antho"} lastname={"LFB"}/>
+        <h2 className='screen-reader-only'>Accounts</h2>
         <Account title={"Argent bank Checking (x8349)"} amount={"2,082.79"} description={"Available Balance"}/>
         <Account title={"Argent Bank Savings (x6712)"} amount={"10,928.42"} description={"Available Balance"}/>
         <Account title={"Argent Bank Credit Card (x8349)"} amount={"184.30"} description={"Current Balance"}/>
