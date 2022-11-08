2fcf686efc1ed015474199bfcacc6bca56e31f6c
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-11-08 15:42:15 +0100

    Import of the component allowing to manage the display of the errors.

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,10 +1,22 @@
 //React
 import React from 'react';
 
+//Components
+import Header from '../../components/Header';
+import DisplayError from '../../components/DisplayError';
+import Footer from '../../components/Footer'
+
+//CSS
+import '../../styles/pages/Error.css';
+
 function Error() {
   return (
     <React.Fragment>
-      <p>Page d'erreur</p>
+      <Header />
+        <main className='main-error'>
+          <DisplayError errorCode={"404"} errorMessage={"The requested content does not exist or is no longer available."}/>
+        </main>
+      <Footer />
     </React.Fragment>
   );
 }
--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -5,21 +5,39 @@ import React from 'react';
 import Header from '../../components/Header';
 import Account from '../../components/Account';
 import Footer from '../../components/Footer';
+import DisplayError from '../../components/DisplayError';
 
 //CSS
 import '../../styles/pages/User.css';
 import UserProfileInformation from '../../components/UserProfileInformation';
 
 function User() {
+
+  //temporary variable to change the state
+  let isUserLoggedfIn = true;
+
+  if(isUserLoggedfIn)
+  {
+    return (
+      <React.Fragment>
+        <Header />
+        <main className='main-user'>
+          <UserProfileInformation name={"Antho"} lastname={"LFB"}/>
+          <h2 className='screen-reader-only'>Accounts</h2>
+          <Account title={"Argent bank Checking (x8349)"} amount={"2,082.79"} description={"Available Balance"}/>
+          <Account title={"Argent Bank Savings (x6712)"} amount={"10,928.42"} description={"Available Balance"}/>
+          <Account title={"Argent Bank Credit Card (x8349)"} amount={"184.30"} description={"Current Balance"}/>
+        </main>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+
   return (
     <React.Fragment>
       <Header />
       <main className='main-user'>
-        <UserProfileInformation name={"Antho"} lastname={"LFB"}/>
-        <h2 className='screen-reader-only'>Accounts</h2>
-        <Account title={"Argent bank Checking (x8349)"} amount={"2,082.79"} description={"Available Balance"}/>
-        <Account title={"Argent Bank Savings (x6712)"} amount={"10,928.42"} description={"Available Balance"}/>
-        <Account title={"Argent Bank Credit Card (x8349)"} amount={"184.30"} description={"Current Balance"}/>
+        <DisplayError errorCode={"401"} errorMessage={"Access denied. You must be logged in to access this content."}/>
       </main>
       <Footer />
     </React.Fragment>
