dc8c653c4fa801bc05951737c7a6ea34c2791492
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-11-15 16:46:26 +0100

    Creation and management of the local state to show or hide the user's profile modification form. The form will be hidden by default when the application is launched.

--- src/components/UserProfileInformation.jsx
+++ src/components/UserProfileInformation.jsx
@@ -1,10 +1,12 @@
+//React
+import { useState } from 'react';
+
 //CSS
 import '../styles/components/UserProfileInformation.css';
 
 function UserProfileInformation({name, lastname})
 {
-    //TEMP
-    let isFormOpen = true;
+    const [isFormOpen, setIsFormOpen] = useState(false);
 
     if(isFormOpen)
     {
@@ -19,7 +21,7 @@ function UserProfileInformation({name, lastname})
                     </div>
                     <div className='profile__update-form__fields-container'>
                         <button className='profile__update-form__fields-container__button' type="submit">Save</button>
-                        <button className='profile__update-form__fields-container__button' type="button">Cancel</button>
+                        <button className='profile__update-form__fields-container__button' type="button"  onClick={ () => setIsFormOpen(false)}>Cancel</button>
                     </div>
                 </form>
             </section>
@@ -29,7 +31,7 @@ function UserProfileInformation({name, lastname})
     return (
         <section className="profile">
             <h1 className="profile__title">Welcome back <br/> {name} {lastname} !</h1>
-            <button className="profile__button" type="button">Edit Name</button>
+            <button className="profile__button" type="button" onClick={ () => setIsFormOpen(true)}>Edit Name</button>
         </section>
     )
 }
