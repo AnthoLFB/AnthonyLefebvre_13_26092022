2b1ddea6b22f0aa3eb625904fbaa2e7069dcb3b3
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-12-15 18:04:07 +0100

    Rework of the redux logic. Some elements have been transferred to the login page and not in the component itself. The redirection is now done from the page.

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,18 @@
 //React
 import React from 'react';
 
+//React
+import { useEffect } from 'react';
+
+//Redux
+import { useSelector } from 'react-redux'
+
+//React-router
+import { useNavigate } from 'react-router-dom';
+
+//Selectors Redux
+import {isUserLoggedIn, isThereAnError} from '../../utils/selectors';
+
 //Components
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
@@ -10,6 +22,18 @@ import LoginForm from '../../components/LoginForm';
 import '../../styles/pages/Login.css';
 
 function Login() {
+
+    const loginStatus = useSelector(isUserLoggedIn);
+    const errorStatus = useSelector(isThereAnError);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(loginStatus && errorStatus == null)
+        {
+            navigate("/profile");
+        }
+      }, [loginStatus, errorStatus, navigate]);
+
   return (
     <React.Fragment>
       <Header />
