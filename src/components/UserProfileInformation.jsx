50854dad89a7b223e78a68c151c90ddeeb7fda2a
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-12-22 18:19:08 +0100

    Added the call to the function when the user clicks on save. Added an error message that is displayed if the form is not filled in correctly.

--- src/components/UserProfileInformation.jsx
+++ src/components/UserProfileInformation.jsx
@@ -1,23 +1,54 @@
 //React
 import { useState } from 'react';
 
+//Redux
+import { useStore } from 'react-redux';
+
+//Redux action
+import { updateUserProfile } from '../features/user';
+
 //CSS
 import '../styles/components/UserProfileInformation.css';
 
-function UserProfileInformation({name, lastname})
+function UserProfileInformation({name, lastname, errorStatus})
 {
+
+    const store = useStore()
+
     const [isFormOpen, setIsFormOpen] = useState(false);
 
     if(isFormOpen)
     {
-        
+        if(errorStatus)
+        {
+            return (
+                <section className="profile">
+                    <h1 className="profile__title">Welcome back</h1>
+                    <form className="profile__update-form" action="" onSubmit={(e) => updateUserProfile(e, store)}>
+                        <div className='profile__update-form__fields-container'>
+                            <input className='profile__update-form__fields-container__input' id="update-form__firstName" type="text" placeholder={name} required/>
+                            <input className='profile__update-form__fields-container__input' id="update-form__lastName" type="text" placeholder={lastname} required/>
+                        </div>
+                        <div className='profile__update-form__fields-container'>
+                            <button className='profile__update-form__fields-container__button' type="submit">Save</button>
+                            <button className='profile__update-form__fields-container__button' type="button"  onClick={ () => setIsFormOpen(false)}>Cancel</button>
+                        </div>
+                    </form>
+
+                    <div className='warning-content'>
+                        <p className='warning-content__message'>{errorStatus.message}</p>
+                    </div>
+                </section>
+            )
+        }
+
         return (
             <section className="profile">
                 <h1 className="profile__title">Welcome back</h1>
-                <form className="profile__update-form" action="" method="POST">
+                <form className="profile__update-form" action="" onSubmit={(e) => updateUserProfile(e, store)}>
                     <div className='profile__update-form__fields-container'>
-                        <input className='profile__update-form__fields-container__input' type="text" placeholder={name} required/>
-                        <input className='profile__update-form__fields-container__input' type="text" placeholder={lastname} required/>
+                        <input className='profile__update-form__fields-container__input' id="update-form__firstName" type="text" placeholder={name} required/>
+                        <input className='profile__update-form__fields-container__input' id="update-form__lastName" type="text" placeholder={lastname} required/>
                     </div>
                     <div className='profile__update-form__fields-container'>
                         <button className='profile__update-form__fields-container__button' type="submit">Save</button>
