3284031d1500a695fb63f64432c2939b810bb4d7
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2023-01-12 14:55:29 +0100

    Errors - Code redundancy. Unnecessary conditions have been removed. The code has been simplified so as not to repeat the HTML code of the component as before. A simple (ternary) conditional operator has been implemented to return the component error or null if there is no error.

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -5,6 +5,7 @@ import { useStore } from 'react-redux';
 //Components
 import CustomInput from "./CustomInput";
 import CustomBtn from "./CustomBtn";
+import Error from './Error';
 
 //CSS
 import '../styles/components/LoginForm.css';
@@ -15,31 +16,10 @@ import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 
 function LoginForm({errorStatus})
 {
-    
     const store = useStore();
     
     // If there is an error when sending the form, or when logging in, this is displayed to the user.
-    if(errorStatus != null)
-    {
-        return (
-            <section className="login">
-                <FontAwesomeIcon icon={faCircleUser}/>
-                <h1 className="login__title">Sign in</h1>
-                <form className="login__form" onSubmit={(e) => getUserToken(e, store)}>
-                    <CustomInput title={"Username"} type={"email"} labelId={"userEmail"} isRequired={true}/>
-                    <CustomInput title={"Password"} type={"password"} labelId={"userPassword"} isRequired={true}/>
-                    <div className="field-container--horizontal">
-                        <input className="field-container__input" type="checkbox" id="backUpUserData" />
-                        <label className="field-container__label" htmlFor="backUpUserData">Remember Me</label>
-                    </div>
-                    <CustomBtn title={"Sign In"} type={"submit"} isActivated={true}/>
-                </form>
-                <div className='warning-content'>
-                    <p className='warning-content__message'>{errorStatus.message}</p>
-                </div>
-            </section>
-        )
-    }
+    const errorToDisplay = (errorStatus ? <Error errorStatus={errorStatus} /> : null);
 
     return (
         <section className="login">
@@ -54,6 +34,7 @@ function LoginForm({errorStatus})
                 </div>
                 <CustomBtn title={"Sign In"} type={"submit"} isActivated={true}/>
             </form>
+            {errorToDisplay}
         </section>
     )
 }
--- src/components/UserProfileInformation.jsx
+++ src/components/UserProfileInformation.jsx
@@ -7,6 +7,9 @@ import { useStore } from 'react-redux';
 //Redux action
 import { updateUserProfile } from '../features/user';
 
+//Components
+import Error from './Error';
+
 //CSS
 import '../styles/components/UserProfileInformation.css';
 
@@ -17,31 +20,11 @@ function UserProfileInformation({name, lastname, errorStatus})
 
     const [isFormOpen, setIsFormOpen] = useState(false);
 
+    // If there is an error when sending the form, or when logging in, this is displayed to the user.
+    const errorToDisplay = (errorStatus ? <Error errorStatus={errorStatus} /> : null);
+
     if(isFormOpen)
     {
-        if(errorStatus)
-        {
-            return (
-                <section className="profile">
-                    <h1 className="profile__title">Welcome back</h1>
-                    <form className="profile__update-form" action="" onSubmit={(e) => updateUserProfile(e, store)}>
-                        <div className='profile__update-form__fields-container'>
-                            <input className='profile__update-form__fields-container__input' id="update-form__firstName" type="text" placeholder={name} required/>
-                            <input className='profile__update-form__fields-container__input' id="update-form__lastName" type="text" placeholder={lastname} required/>
-                        </div>
-                        <div className='profile__update-form__fields-container'>
-                            <button className='profile__update-form__fields-container__button' type="submit">Save</button>
-                            <button className='profile__update-form__fields-container__button' type="button"  onClick={ () => setIsFormOpen(false)}>Cancel</button>
-                        </div>
-                    </form>
-
-                    <div className='warning-content'>
-                        <p className='warning-content__message'>{errorStatus.message}</p>
-                    </div>
-                </section>
-            )
-        }
-
         return (
             <section className="profile">
                 <h1 className="profile__title">Welcome back</h1>
@@ -55,6 +38,7 @@ function UserProfileInformation({name, lastname, errorStatus})
                         <button className='profile__update-form__fields-container__button' type="button"  onClick={ () => setIsFormOpen(false)}>Cancel</button>
                     </div>
                 </form>
+                {errorToDisplay}
             </section>
         )
     }
