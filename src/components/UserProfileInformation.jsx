263cfbe41eb069c81693e70b2a67bf8c3d4221c7
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2023-01-12 18:00:21 +0100

    DisplayForm - The appearance and disappearance of elements is done by CSS class and not by HTML.

--- src/components/UserProfileInformation.jsx
+++ src/components/UserProfileInformation.jsx
@@ -15,7 +15,6 @@ import '../styles/components/UserProfileInformation.css';
 
 function UserProfileInformation({name, lastname, errorStatus})
 {
-
     const store = useStore()
 
     const [isFormOpen, setIsFormOpen] = useState(false);
@@ -23,30 +22,23 @@ function UserProfileInformation({name, lastname, errorStatus})
     // If there is an error when sending the form, or when logging in, this is displayed to the user.
     const errorToDisplay = (errorStatus ? <Error errorStatus={errorStatus} /> : null);
 
-    if(isFormOpen)
-    {
-        return (
-            <section className="profile">
-                <h1 className="profile__title">Welcome back</h1>
-                <form className="profile__update-form" action="" onSubmit={(e) => updateUserProfile(e, store)}>
-                    <div className='profile__update-form__fields-container'>
-                        <input className='profile__update-form__fields-container__input' id="update-form__firstName" type="text" placeholder={name} required/>
-                        <input className='profile__update-form__fields-container__input' id="update-form__lastName" type="text" placeholder={lastname} required/>
-                    </div>
-                    <div className='profile__update-form__fields-container'>
-                        <button className='profile__update-form__fields-container__button' type="submit">Save</button>
-                        <button className='profile__update-form__fields-container__button' type="button"  onClick={ () => setIsFormOpen(false)}>Cancel</button>
-                    </div>
-                </form>
-                {errorToDisplay}
-            </section>
-        )
-    }
-
     return (
         <section className="profile">
-            <h1 className="profile__title">Welcome back <br/> {name} {lastname} !</h1>
-            <button className="profile__button" type="button" onClick={ () => setIsFormOpen(true)}>Edit Name</button>
+            <h1 className="profile__title">Welcome back</h1>
+            <h2 className={'profile__title display-' + !isFormOpen}> {name} {lastname} !</h2>
+            <button className={"profile__button display-" + !isFormOpen} type="button" onClick={ () => setIsFormOpen(true)}>Edit Name</button>
+
+            <form className={"profile__update-form display-" + isFormOpen} onSubmit={(e) => updateUserProfile(e, store)}>
+                <div className='profile__update-form__fields-container'>
+                    <input className='profile__update-form__fields-container__input' id="update-form__firstName" type="text" placeholder={name} required/>
+                    <input className='profile__update-form__fields-container__input' id="update-form__lastName" type="text" placeholder={lastname} required/>
+                </div>
+                <div className='profile__update-form__fields-container'>
+                    <button className='profile__update-form__fields-container__button' type="submit">Save</button>
+                    <button className='profile__update-form__fields-container__button' type="button"  onClick={ () => setIsFormOpen(false)}>Cancel</button>
+                </div>
+                {errorToDisplay}
+            </form>
         </section>
     )
 }
