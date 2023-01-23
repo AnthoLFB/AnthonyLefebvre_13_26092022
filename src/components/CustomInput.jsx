0cb2cb5757dee93a1250e1a7d0c9ba56eb38001a
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2023-01-23 16:20:59 +0100

    Code replacement to handle the required attribute with a ternary.

--- src/components/CustomInput.jsx
+++ src/components/CustomInput.jsx
@@ -3,20 +3,13 @@ import '../styles/components/CustomInput.css';
 
 function CustomInput({title, type, labelId, isRequired})
 {
-    if(isRequired)
-    {
-        return (
-            <div className="field-container">
-                <label className="field-container__label" htmlFor={labelId}>{title}</label>
-                <input className="field-container__input" type={type} id={labelId} required/>
-            </div>
-        )
-    }
+
+    let input = isRequired ? <input className="field-container__input" type={type} id={labelId} required/> : <input className="field-container__input" type={type} id={labelId}/>;
 
     return (
         <div className="field-container">
             <label className="field-container__label" htmlFor={labelId}>{title}</label>
-            <input className="field-container__input" type={type} id={labelId} />
+            {input}
         </div>
     )
 }
